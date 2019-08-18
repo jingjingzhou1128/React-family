6107722d81dd2513115a5431204025a96492ed1e
jingjingzhou1128 <ZJJ_zjj1128@163.com>
2019-08-18 10:38:48 +0800

    sidebar

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
-import {HashRouter as Router, Link, Switch, Route} from 'react-router-dom';
-import {asyncComponent} from '@/App.js'
+// import {HashRouter as Router, Link, Switch, Route} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+// import {asyncComponent} from '@/App.js'
 
 export default class Layout extends Component {
   render () {
+    console.log(this.props)
     return (
-      <Router>
-        <div>
-          <Link to="/home/dashboard">Dashboard</Link>
-          <p>Home</p>
-          <Switch>
-            <Route path="/home/dashboard" component={asyncComponent(() => import('@/pages/dashboard/index.js'))}/>
-          </Switch>
-        </div>
-      </Router>
+      <div className="app">
+        <aside className="sidebar">
+          <ul>
+            <li><Link to="/dashboard">Dashboard</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+          </ul>
+        </aside>
+        <main>
+          <header>
+            <div>Header</div>
+          </header>
+          <div className="main-container">
+            {this.props.children}
+          </div>
+        </main>
+      </div>
+      // <Router>
+      //   <div>
+      //     <Link to="/home/dashboard">Dashboard</Link>
+      //     <p>Home</p>
+      //     <Switch>
+      //       <Route path="/home/dashboard" component={asyncComponent(() => import('@/pages/dashboard/index.js'))}/>
+      //     </Switch>
+      //   </div>
+      // </Router>
     )
   }
 }
