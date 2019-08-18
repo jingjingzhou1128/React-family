9d6e6ce7213a1583f552f5ec767495a82fcab9e7
jingjingzhou1128 <ZJJ_zjj1128@163.com>
2019-08-18 11:49:18 +0800

    add

--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {Link, Route, Switch} from 'react-router-dom'
+import {asyncComponent} from '@/App.js'
 
 export default class Menu extends Component {
   render () {
